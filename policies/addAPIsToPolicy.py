ad1aed9eca65989592cee50f50ef4064d351a6ca
Peter Stubbs <peter@tyk.io>
2022-06-11 16:44:49 +0100

    addAPIsToPolicy.py tidyup and improved messages

--- policies/addAPIsToPolicy.py
+++ policies/addAPIsToPolicy.py
@@ -11,21 +11,17 @@ import sys
 scriptName = os.path.basename(__file__)
 
 def printhelp():
-    print(f'{scriptName} --policy <policy ID> --dashboard <dashboard URL> --cred <Dashboard API credentials> --number <number of APIs to add to the policy>')
+    print(f'{scriptName} --policy <policy ID> --dashboard <dashboard URL> --cred <Dashboard API credentials> --number <number of APIs to add to the policy> --verbose')
     sys.exit(2)
-
-#dshb = "http://10.0.0.21:3002"
-#auth = "c384aaccf24242165405dac097248dca"
-#policyID = "62a487c4411022005169806e"
-#toAdd = 100
 
 dshb = ""
 auth = ""
 policyID = ""
 toAdd = 0
+verbose = 0
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "policy=", "dashboard=", "cred=", "number="])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "policy=", "dashboard=", "cred=", "number=", "verbose"])
 except getopt.GetoptError:
     printhelp()
 
@@ -40,6 +36,8 @@ for opt, arg in opts:
         auth = arg
     elif opt == '--number':
         toAdd = int(arg)
+    elif opt == '--verbose':
+        verbose = 1
 
 if not (dshb or policyID or auth or toAdd):
     printhelp()
@@ -48,23 +46,21 @@ if not (dshb or policyID or auth or toAdd):
 headers = {'Authorization' : auth}
 resp = requests.get(f'{dshb}/api/portal/policies/{policyID}', headers=headers)
 policy = json.loads(resp.text)
-#print(json.dumps(policy, indent=4, sort_keys=True))
+if verbose:
+    keycount = len(policy["access_rights"])
+    print(f'Policy {policyID} has {keycount} APIs attached')
 # get the APIs
 resp = requests.get(f'{dshb}/api/apis/?p=-1', headers=headers)
 apis = json.loads(resp.text)
 # print(json.dumps(apis, indent=4, sort_keys=True))
 addedCount = 0
 for api in apis['apis']:
-    #print(json.dumps(api['api_definition'], indent=4, sort_keys=True))
     apiid=api["api_definition"]["api_id"]
     apiName = api["api_definition"]["name"]
-    #print(f'ID: {apiid}')
-    #print(f'Name: {apiName}')
-    #for key in policy["access_rights"].keys():
-    #    print(f'Key = {key}')
     if addedCount < toAdd:
         if not apiid in policy["access_rights"].keys():
-            print(f'Adding {apiid}, {apiName}')
+            if verbose:
+                print(f'Adding {apiid}, {apiName}')
             policy["access_rights"][apiid] = {
                 "api_name": apiName,
                 "api_id": apiid,
@@ -89,12 +85,15 @@ for api in apis['apis']:
             })
             addedCount+=1
         else:
-            print(f'Skipping {apiid}, {apiName}')
+            if verbose:
+                print(f'Skipping {apiid}, {apiName}, already attached')
+    else:
+        continue
 if addedCount < toAdd:
     print(f'Only able to add {addedCount} APIs because because there too fews APIs defined')
+print(f'Policy {policyID} will have a total of {len(policy["access_rights"])} APIs attached')
 #print(json.dumps(policy, indent=4, sort_keys=True))
 print("Uploading policy to dashboard")
 headers["Content-Type"] = "application/json"
 resp = requests.put(f'{dshb}/api/portal/policies/{policyID}', data=json.dumps(policy, indent=4), headers=headers)
-print(json.dumps(resp.text, indent=4, sort_keys=True))
-exit(0)
+print(resp.text)
