28a6d557af85adef3acce05c5a99e6261a5f2a3f
Peter Stubbs <peter@tyk.io>
2022-08-14 14:25:10 +0100

    checkKeysAndPolicies.py: MVP?

--- checks/checkKeysAndPolicies.py
+++ checks/checkKeysAndPolicies.py
@@ -19,6 +19,21 @@ auth = ""
 
 scriptName = os.path.basename(__file__)
 
+def check_access_rights(name, ID, access_rights_array, access_rights):
+    API_IDs = {}
+    # check that all APIs in access_rights_array also exist in access_rights
+    for API in access_rights_array:
+        api_id = API['api_id']
+        if not api_id in access_rights:
+            print(f"[WARN]{name} {ID}: API {api_id} in access_rights_array but not in access_rights")
+        # keep the api_id to make checking in reverse easier
+        API_IDs[api_id] = 1
+    # check that all APIs in access_rights are present in access_rights_array (actually API_IDs since that's easier)
+    for api_id in access_rights:
+        if not api_id in API_IDs:
+            print(f"[WARN]{name} {ID}: API {api_id} in access_rights but not in access_rights_array")
+
+
 def printhelp():
     print(f'{scriptName} --dashboard <dashboard_URL> --cred <Dashboard API credentials> [--verbose]')
     sys.exit(2)
@@ -49,26 +64,31 @@ auth_header = {'Authorization' : auth}
 # /api/keys/detailed/?p=-1 which dump the details of all the keys
 resp = requests.get(f'{dshb}/api/keys/detailed/?p=-1', headers=auth_header)
 keys = json.loads(resp.text)
+# get policies
+resp = requests.get(f'{dshb}/api/portal/policies/?p=-1', headers=auth_header)
+policies = json.loads(resp.text)
+# get APIs
+resp = requests.get(f'{dshb}/api/apis/?p=-1', headers=auth_header)
+APIs = json.loads(resp.text)
+
+# load from files:
+#f = open ('keys', "r")
+#keys = json.loads(f.read())
+#f.close()
+#f = open ('policies', "r")
+#policies = json.loads(f.read())
+#f.close()
+#f = open ('APIs', "r")
+#APIs = json.loads(f.read())
+#f.close()
 
 if verbose:
     print("List of all keys:")
     for key in keys['keys']:
         print(key['key_id'])
-
-# get policies
-resp = requests.get(f'{dshb}/api/portal/policies/?p=-1', headers=auth_header)
-policies = json.loads(resp.text)
-
-if verbose:
     print("List of all policies:")
     for policy in policies['Data']:
         print(policy['name'])
-
-# get APIs
-resp = requests.get(f'{dshb}/api/apis/?p=-1', headers=auth_header)
-APIs = json.loads(resp.text)
-
-if verbose:
     print("List of all APIs:")
     for API in APIs['apis']:
         print(API['api_definition']['name'], API['api_definition']['api_id'])
@@ -87,7 +107,8 @@ for key in keys['keys']:
         api_id = keyAPI['api_id'] 
 
         # Check that all APIs in the keys exist
-        print(f"KEY: Checking {api_id}")
+        if verbose:
+            print(f"[INFO]Key: {key_id}, checking API {api_id}")
         if not api_id in APIdetails:
             print(f"[FATAL]Key {key_id} refers to deleted API {api_id}")
         else:
@@ -103,6 +124,9 @@ for key in keys['keys']:
             if key_org != api_org:
                 print(f"[FATAL]Key is in org {key_org} but API is in org {api_org}")
 
+    # Check that Key access_rights_array and access_rights match?
+    check_access_rights("Key", key_id, key['data']['access_rights_array'], key['data']['access_rights'])
+
 # Policy checks
 for policy in policies['Data']:
     policy_id = policy['_id']
@@ -111,7 +135,8 @@ for policy in policies['Data']:
         api_id = policyAPI['api_id'] 
 
         # Check that all APIs in the policy exist
-        print(f"POL: Checking {api_id}")
+        if verbose:
+            print(f"[INFO]Policy: {policy_id}, checking API {api_id}")
         if not api_id in APIdetails:
             print(f"[FATAL]Policy {policy_id} refers to deleted API {api_id}")
         else:
@@ -127,5 +152,5 @@ for policy in policies['Data']:
             if policy_org != api_org:
                 print(f"[FATAL]Policy is in org {policy_org} but API is in org {api_org}")
 
-# Check that access_rights_array and access_rights match?
-
+    # Check that policy access_rights_array and access_rights match?
+    check_access_rights("Policy", policy_id, policy['access_rights_array'], policy['access_rights'])
