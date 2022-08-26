7a95a231996de943721342d645a82f3ab27f22c6
Peter Stubbs <peter@tyk.io>
2022-08-26 08:36:47 +0100

    checkCORS.py: Attempt to capture my current understanding of CORS

---
+++ checks/checkCORS.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+# The intent of this script is to check the CORS headers returned
+# Checks include
+#import json
+
+import requests
+import sys
+import getopt
+import os
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# globals
+url = ""
+origins = ""
+verbose = 0
+
+scriptName = os.path.basename(__file__)
+
+def printhelp():
+    print(f'{scriptName} --url <Rest URL> --origins <list of origins to try>')
+    sys.exit(2)
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "url=", "origins="])
+except getopt.GetoptError:
+    printhelp()
+
+for opt, arg in opts:
+    if opt == '--help':
+        printhelp()
+    elif opt == '--url':
+        #url = arg.strip().strip('/')
+        url = arg
+    elif opt == '--verbose':
+        verbose = 1
+    elif opt == '--origins':
+        origins = arg.split(",")
+
+if not (url or len(origins)):
+    printhelp()
+
+origin_header = {'Origin' : 'http://localhost'}
+method_header = {'Access-control-request-method': 'GET'}
+
+for origin in origins:
+    print(f"Testing Origin: {origin}")
+    for method in ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'HEAD', 'CONNECT', 'TRACE']:
+        resp = requests.options(f'{url}', headers={'Origin' : origin, 'Access-control-request-method': method}, verify=False)
+        #print(resp.headers)
+        if "Access-Control-Allow-Methods" in resp.headers:
+            print(f"    Method allowed: {method}")
