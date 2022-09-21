31a5ed3b2c7e931f536ce9dd212617284eb29c0d
Peter Stubbs <peter@tyk.io>
2022-09-21 17:16:43 -0500

    checkCORS.py: added verbose option

--- checks/checkCORS.py
+++ checks/checkCORS.py
@@ -19,11 +19,11 @@ verbose = 0
 scriptName = os.path.basename(__file__)
 
 def printhelp():
-    print(f'{scriptName} --url <Rest URL> --origins <list of origins to try>')
+    print(f'{scriptName} --url <Rest URL> --origins <list of origins to try> [--verbose]')
     sys.exit(2)
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "url=", "origins="])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "url=", "origins=", "verbose"])
 except getopt.GetoptError:
     printhelp()
 
@@ -42,21 +42,26 @@ if not (url or len(origins)):
     printhelp()
 
 for origin in origins:
-    print(f"Testing Origin: {origin}")
+    print(f'Testing Origin: {origin}')
     for method in ['GET', 'PUT', 'POST', 'OPTIONS', 'DELETE', 'HEAD', 'CONNECT', 'TRACE']:
-        #resp = requests.options(f'{url}', headers={'Origin' : origin, 'Access-control-request-method': method, 'Access-control-request-headers': 'origin'}, verify=False)
         resp = requests.options(f'{url}', headers={'Origin' : origin, 'Access-control-request-method': method}, verify=False)
-        #print(resp.headers)
+        code = resp.status_code
 
         # Current understading only, might be wrong:
         #  if Access-Control-Allow-Origin is populated, it has to contain the domain from Origin, '*' isn't good enough
         if 'Access-Control-Allow-Origin' in resp.headers:
-            #print(resp.headers['Access-Control-Allow-Origin'])
             if resp.headers['Access-Control-Allow-Origin'] == origin:
-                if "Access-Control-Allow-Methods" in resp.headers:
-                    print(f"    Method allowed: {method}")
-        else:
-            # if Access-Control-Allow-Origin doesn't exist, just do with the answer in Access-Control-Allow-Methods
+                if 'Access-Control-Allow-Methods' in resp.headers:
+                    if verbose:
+                        print(f'    Method allowed: {method} , OPTIONS call status:{code}')
+                    else:
+                        print(f'    Method allowed: {method}')
+        elif 'Access-Control-Allow-Methods' in resp.headers:
+            # if Access-Control-Allow-Origin doesn't exist, just go with the answer in Access-Control-Allow-Methods
             # I don't know if this is correct or not
-            if "Access-Control-Allow-Methods" in resp.headers:
-                print(f"    Method allowed: {method}")
+            if verbose:
+                print(f'    Method allowed: {method} , OPTIONS call status:{code}')
+            else:
+                print(f'    Method allowed: {method}')
+        elif verbose:
+                print(f"    Disallowed by CORS: {method}")
