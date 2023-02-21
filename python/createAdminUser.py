9613eff0758d6109acb562dcf03793ce9ec3dcaa
Peter Stubbs <peter@tyk.io>
2023-02-21 10:59:47 +0000

    createAdminUser.py: example call

---
+++ python/createAdminUser.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+###############################################################
+# Reference for calling the dashboard.createAdminUser
+# arguments are dashboard, dashboard API key, admin secret, orgid, username, user password
+###############################################################
+
+import json
+import requests
+import os
+import getopt
+import sys
+import time
+sys.path.append(f'{os.environ.get("HOME")}/code/tyk-scripts/module')
+import tyk
+
+# Suppress the warnings from urllib3 when using a self signed certs
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+scriptName = os.path.basename(__file__)
+
+def printhelp():
+    print(f'{scriptName} --dashboard <dashboard URL> URL> --adminsecret <admin secret> --useremail <user email> --userpass <user password>')
+    print("    Create an admin user in all orgs in the dashboard instance")
+    sys.exit(1)
+
+dshb = ""
+auth = ""
+useremail = ""
+userpass = ""
+verbose = 0
+adminsecret = ""
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "dashboard=", "adminsecret=", "useremail=", "userpass=", "number=", "verbose"])
+except getopt.GetoptError:
+    printhelp()
+
+for opt, arg in opts:
+    if opt == '--help':
+        printhelp()
+    elif opt == '--dashboard':
+        dshb = arg.strip().strip('/')
+    elif opt == '--adminsecret':
+        adminsecret = arg
+    elif opt == '--useremail':
+        useremail = arg
+    elif opt == '--userpass':
+        userpass = arg
+    elif opt == '--verbose':
+        verbose = 1
+
+if not (dshb or useremail or userpass or adminsecret):
+    printhelp()
+
+dashboard = tyk.dashboard(dshb, "", adminsecret)
+
+organisations = dashboard.getOrganisations().json()
+for org in organisations["organisations"]:
+    resp = dashboard.createAdminUser(useremail, userpass, org["id"])
+    print(resp.json())
