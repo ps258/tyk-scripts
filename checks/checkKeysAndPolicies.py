31153e79f10f470082b3154c892c915225c24f21
Peter Stubbs <peter@tyk.io>
2023-07-28 14:57:58 +0100

    ../checks/checkKeysAndPolicies.py: not sure if this is a full update or not :(

--- checks/checkKeysAndPolicies.py
+++ checks/checkKeysAndPolicies.py
@@ -16,33 +16,20 @@ import tyk
 
 # globals
 dshb = ""
+gatw = ""
 verbose = 0
 auth = ""
 
 scriptName = os.path.basename(__file__)
 
-def check_access_rights(name, ID, access_rights_array, access_rights):
-    API_IDs = {}
-    # check that all APIs in access_rights_array also exist in access_rights
-    for API in access_rights_array:
-        api_id = API['api_id']
-        if not api_id in access_rights:
-            print(f"[WARN]{name} {ID}: API {api_id} in access_rights_array but not in access_rights")
-        # keep the api_id to make checking in reverse easier
-        API_IDs[api_id] = 1
-    # check that all APIs in access_rights are present in access_rights_array (actually API_IDs since that's easier)
-    for api_id in access_rights:
-        if not api_id in API_IDs:
-            print(f"[WARN]{name} {ID}: API {api_id} in access_rights but not in access_rights_array")
-
-
 def printhelp():
-    print(f'{scriptName} --dashboard <dashboard_URL> --cred <Dashboard API credentials> [--verbose]')
+    print(f'{scriptName} [--dashboard <dashboard URL>|--gateway <gateway URL>] --cred <Dashboard API key|Gateway secret>')
     sys.exit(2)
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "dashboard=", "cred=", "verbose"])
-except getopt.GetoptError:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "dashboard=", "gateway=", "cred=", "verbose"])
+except getopt.GetoptError as opterr:
+    print(f'Error in option: {opterr}')
     printhelp()
 
 for opt, arg in opts:
@@ -50,26 +37,32 @@ for opt, arg in opts:
         printhelp()
     elif opt == '--dashboard':
         dshb = arg.strip().strip('/')
+    elif opt == '--gateway':
+        gatw = arg.strip().strip('/')
     elif opt == '--cred':
         auth = arg
     elif opt == '--verbose':
         verbose = 1
 
-if not (dshb or auth ):
+if not ((dshb or gatw) and auth ):
     printhelp()
 
-dashboard = tyk.dashboard(dshb, auth)
+# create a new dashboard or gateway object
+if dshb:
+    tyk = tyk.dashboard(dshb, auth)
+else:
+    tyk = tyk.gateway(gatw, auth)
 
 # get keys
 # /api/apis/keys/?p=-1 which just lists the key ids
 # /api/keys/detailed/?p=-1 which dump the details of all the keys
-resp = dashboard.getKeys()
+resp = tyk.getKeys()
 keys = resp.json()
 # get policies
-resp = dashboard.getPolicies()
+resp = tyk.getPolicies()
 policies = resp.json()
 # get APIs
-resp = dashboard.getAPIs()
+resp = tyk.getAPIs()
 APIs = resp.json()
 if verbose:
     print(f'keys={keys}')
@@ -98,7 +91,7 @@ for key in keys['keys']:
     key_id = key['key_id']
     key_org = key['data']['org_id']
     for keyAPI in key['data']['access_rights_array']:
-        api_id = keyAPI['api_id'] 
+        api_id = keyAPI['api_id']
 
         # Check that all APIs in the keys exist
         if verbose:
@@ -118,15 +111,14 @@ for key in keys['keys']:
             if key_org != api_org:
                 print(f"[FATAL]Key is in org {key_org} but API is in org {api_org}")
 
-    # Check that Key access_rights_array and access_rights match?
-    check_access_rights("Key", key_id, key['data']['access_rights_array'], key['data']['access_rights'])
 
 # Policy checks
 for policy in policies:
     policy_id = policy['_id']
     policy_org = policy['org_id']
-    for policyAPI in policy['access_rights_array']:
-        api_id = policyAPI['api_id'] 
+    #print(json.dumps(policy['access_rights'], indent=2))
+    for policyAPI in policy['access_rights']:
+        api_id = policy['access_rights'][policyAPI]['api_id']
 
         # Check that all APIs in the policy exist
         if verbose:
@@ -145,6 +137,3 @@ for policy in policies:
             api_org = APIdetails[api_id]['org_id']
             if policy_org != api_org:
                 print(f"[FATAL]Policy is in org {policy_org} but API is in org {api_org}")
-
-    # Check that policy access_rights_array and access_rights match?
-    check_access_rights("Policy", policy_id, policy['access_rights_array'], policy['access_rights'])
