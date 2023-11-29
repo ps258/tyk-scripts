a4e1906c5f98e4175c42c2ee67d7355fc6383579
Peter Stubbs <peter@tyk.io>
2023-11-29 19:43:49 +0000

    createKeysFromPolicy.py: added support for creating multiple keys

--- python/createKeysFromPolicy.py
+++ python/createKeysFromPolicy.py
@@ -10,17 +10,18 @@ import tyk
 scriptName = os.path.basename(__file__)
 
 def printhelp():
-    print(f'{scriptName} --policy <policy ID> --dashboard <dashboard URL> --cred <Dashboard API credentials> --verbose')
-    print("    Will create a key from the given policy taking the defaults from it")
+    print(f'{scriptName} --policy <policy ID, policy ID> --dashboard <dashboard URL> --cred <Dashboard API credentials> --verbose --number = <number of keys to create>')
+    print("    Will create keys from the given policy or policies taking the defaults from them")
     sys.exit(1)
 
 dshb = ""
 auth = ""
 policyID = ""
 verbose = 0
+number = 1
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "policy=", "dashboard=", "cred=", "verbose"])
+    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "policy=", "dashboard=", "cred=", "number=", "verbose"])
 except getopt.GetoptError as opterr:
     print(f'Error in option: {opterr}')
     printhelp()
@@ -32,12 +33,14 @@ for opt, arg in opts:
         policyID = arg
     elif opt == '--dashboard':
         dshb = arg.strip().strip('/')
+    elif opt == '--number':
+        number = int(arg)
     elif opt == '--cred':
         auth = arg
     elif opt == '--verbose':
         verbose = 1
 
-if not (dshb or policyID or auth):
+if not (dshb and policyID and auth):
     printhelp()
 
 dashboard = tyk.dashboard(dshb, auth)
@@ -53,11 +56,13 @@ TykKey = {
 
 for pol in policyID.split(","):
     TykKey["apply_policies"].append(pol)
-resp = dashboard.createKey(json.dumps(TykKey))
 
-if verbose:
-    print(json.dumps(resp.json()), indent=2)
-else:
-    print(resp.json()["key_id"])
-if resp.status_code != 200:
-    sys.exit(1)
+for x in range(number):
+    resp = dashboard.createKey(json.dumps(TykKey))
+
+    if verbose:
+        print(json.dumps(resp.json()), indent=2)
+    else:
+        print(resp.json()["key_id"])
+    if resp.status_code != 200:
+        sys.exit(1)
