3800542b014ae1fbf3342ec48164bd13e80755cc
Peter Stubbs <peter@tyk.io>
2024-05-10 16:00:55 +0100

    ../ratelimits/callRatesFromAnalytics.py: added support for multiple APIs at once

--- ratelimits/callRatesFromAnalytics.py
+++ ratelimits/callRatesFromAnalytics.py
@@ -11,13 +11,13 @@ import datetime
 scriptName = os.path.basename(__file__)
 
 def printhelp():
-    print(f'{scriptName} --dashboard <dashboard URL> --cred <Dashboard API key or Gateway secret> --start <start epoch second> --end <end epoch second> --apiid <apiid>')
+    print(f'{scriptName} --dashboard <dashboard URL> --cred <Dashboard API key or Gateway secret> --start <start epoch second> --end <end epoch second> --apiid <apiid,apiid,apiid...>')
     print("    prints the number of http response codes issued per second")
     sys.exit(1)
 
 dshb = ""
 auth = ""
-apiid = ""
+apiids = ""
 verbose = 0
 start = int(time.time())
 end = int(time.time()) - 600 # the last 10 minutes
@@ -40,38 +40,39 @@ for opt, arg in opts:
     elif opt == '--end':
         end = int(arg)
     elif opt == '--apiid':
-        apiid = arg
+        apiids = arg
     elif opt == '--verbose':
         verbose = 1
 
-if not (dshb and auth and start and end and apiid):
+if not (dshb and auth and start and end and apiids):
     printhelp()
 
 if end <= start:
     print('[FATAL]--end must be after --start')
 
 
-print(f'Calling {dshb}/api/logs?start={start}&end={end}&p=-1&api={apiid}')
-resp = requests.get(f'{dshb}/api/logs?start={start}&end={end}&p=-1&api={apiid}', headers={'Authorization': auth}, verify=False)
-if resp.status_code != 200:
-    print(resp)
-    sys.exit(1)
-
-analytics = resp.json()['data']
 results = dict()
-for log in analytics:
-    try:
-        timestamp = datetime.datetime.strptime(log['TimeStamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
-    except:
-        timestamp = datetime.datetime.strptime(log['TimeStamp'], "%Y-%m-%dT%H:%M:%SZ")
-    epoch_time = int(timestamp.timestamp())
-    # it might be an idea to make sure that there upstream latency is 0 here.
-    # non-zero latencies come from upstream so 429s should all have 0 latency
-    if not epoch_time in results:
-        results[epoch_time] = dict()
-    if not log['ResponseCode'] in results[epoch_time]:
-        results[epoch_time][log['ResponseCode']] = 0
-    results[epoch_time][log['ResponseCode']] += 1
+for apiid in apiids.split(','):
+    print(f'Calling {dshb}/api/logs?start={start}&end={end}&p=-1&api={apiid}')
+    resp = requests.get(f'{dshb}/api/logs?start={start}&end={end}&p=-1&api={apiid}', headers={'Authorization': auth}, verify=False)
+    if resp.status_code != 200:
+        print(resp)
+        sys.exit(1)
+
+    analytics = resp.json()['data']
+    for log in analytics:
+        try:
+            timestamp = datetime.datetime.strptime(log['TimeStamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        except:
+            timestamp = datetime.datetime.strptime(log['TimeStamp'], "%Y-%m-%dT%H:%M:%SZ")
+        epoch_time = int(timestamp.timestamp())
+        # it might be an idea to make sure that there upstream latency is 0 here.
+        # non-zero latencies come from upstream so 429s should all have 0 latency
+        if not epoch_time in results:
+            results[epoch_time] = dict()
+        if not log['ResponseCode'] in results[epoch_time]:
+            results[epoch_time][log['ResponseCode']] = 0
+        results[epoch_time][log['ResponseCode']] += 1
 
 for epoch_time in sorted(results.keys()):
     first = True
