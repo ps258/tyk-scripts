e5ad81f417c8dade035f9eb123af07e6a409cb73
Peter Stubbs <peter@tyk.io>
2024-05-28 12:15:12 +0100

    createCert.py: moved to argparse

--- python/createCert.py
+++ python/createCert.py
@@ -1,56 +1,36 @@
 #!/usr/bin/python3
 
+import argparse
 import json
 import os
-import getopt
 import sys
 sys.path.append(f'{os.path.abspath(os.path.dirname(__file__))}/module')
 import tyk
 
 scriptName = os.path.basename(__file__)
 
-def printhelp():
-    print(f'{scriptName} [--dashboard <dashboard URL>|--gateway <gateway URL>] --cred <Dashboard API key or Gateway secret> --certificate <PEM format file> --verbose')
-    print("    Will add the certificate to the certificate store")
-    sys.exit(1)
+parser = argparse.ArgumentParser(description=f'{scriptName}: Will add the certificate to the certificate store')
 
-dshb = ""
-gatw = ""
-auth = ""
-certFile = ""
-verbose = 0
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-d', '--dashboard', dest='dshb', help="URL of the dashboard")
+group.add_argument('-g', '--gateway', dest='gatw', help="URL of the gateway")
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "certificate=", "dashboard=", "gateway=", "cred=", "verbose"])
-except getopt.GetoptError as opterr:
-    print(f'Error in option: {opterr}')
-    printhelp()
+parser.add_argument('-c', '--cred', required=True, dest='auth', help="Dashboard API key or Gateway secret")
+parser.add_argument('-C', '--certificate', required=True, dest='certFile', help="PEM format file")
+parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help="Verbose output")
 
-for opt, arg in opts:
-    if opt == '--help':
-        printhelp()
-    elif opt == '--certificate':
-        certFile = arg
-    elif opt == '--dashboard':
-        dshb = arg.strip().strip('/')
-    elif opt == '--gateway':
-        gatw = arg.strip().strip('/')
-    elif opt == '--cred':
-        auth = arg
-    elif opt == '--verbose':
-        verbose = 1
-
-if not ((dshb or gatw) and certFile and auth):
-    printhelp()
+args = parser.parse_args()
 
 # create a new dashboard or gateway object
-if dshb:
-    tykInstance = tyk.dashboard(dshb, auth)
+if args.dshb:
+    args.dshb = args.dshb.strip('/')
+    tykInstance = tyk.dashboard(args.dshb, args.auth)
 else:
-    tykInstance = tyk.gateway(gatw, auth)
+    args.gatw = args.gatw.strip('/')
+    tykInstance = tyk.gateway(args.gatw, args.auth)
 
 
-resp = tykInstance.createCert(certFile)
+resp = tykInstance.createCert(args.certFile)
 print(json.dumps(resp.json()))
 if resp.status_code != 200:
     print(f'[FATAL]Tyk returned {resp.status_code}', file=sys.stderr)
