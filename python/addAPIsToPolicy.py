41538c8589e3af2e873e6f49d81e9e3929376ce0
Peter Stubbs <peter@tyk.io>
2024-05-28 10:47:14 +0100

    ./addAPIsToPolicy.py: moved to argparse

--- python/addAPIsToPolicy.py
+++ python/addAPIsToPolicy.py
@@ -1,59 +1,37 @@
 #!/usr/bin/python3
 
+import argparse
 import json
 import os
-import getopt
 import sys
 sys.path.append(f'{os.path.abspath(os.path.dirname(__file__))}/module')
 import tyk
 
 scriptName = os.path.basename(__file__)
 
-def printhelp():
-    print(f'{scriptName} --policy <policy ID> --dashboard <dashboard URL> --cred <Dashboard API credentials> --number <number of APIs to add to the policy> --verbose')
-    print("    Will add any available APIs into the policy named. There is no way to select which APIs are added")
-    sys.exit(1)
+parser = argparse.ArgumentParser(description=f'{scriptName}: Will add any available APIs into the policy named. There is no way to select which APIs are added ')
 
-dshb = ""
-auth = ""
-policyID = ""
-toAdd = 0
-verbose = 0
+parser.add_argument('-d', '--dashboard', required=True, dest='dshb', help="URL of the dashboard")
+parser.add_argument('-p', '--policy', required=True, dest='policy', help="Policy ID to add APIs to")
+parser.add_argument('-c', '--cred', required=True, dest='auth', help="Access credential")
+parser.add_argument('-n', '--number', required=True, type=int, dest='toAdd', help="Number of APIs to add to the policy")
+parser.add_argument('-v', '--verbose', required=False, type=bool, dest='verbose', help="Verbose output")
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["help", "policy=", "dashboard=", "cred=", "number=", "verbose"])
-except getopt.GetoptError as opterr:
-    print(f'Error in option: {opterr}')
-    printhelp()
+args = parser.parse_args()
+args.dshb = args.dshb.strip('/')
 
-for opt, arg in opts:
-    if opt == '--help':
-        printhelp()
-    elif opt == '--policy':
-        policyID = arg
-    elif opt == '--dashboard':
-        dshb = arg.strip().strip('/')
-    elif opt == '--cred':
-        auth = arg
-    elif opt == '--number':
-        toAdd = int(arg)
-    elif opt == '--verbose':
-        verbose = 1
 
-if not (dshb and policyID and auth and toAdd):
-    printhelp()
-
-tykInstance = tyk.dashboard(dshb, auth)
+tykInstance = tyk.dashboard(args.dshb, args.auth)
 
 # get the polcy
-policy = tykInstance.getPolicy(policyID).json()
-if verbose:
+policy = tykInstance.getPolicy(args.policyID).json()
+if args.verbose:
     keycount = len(policy["access_rights"])
     print(f'Policy {policyID} has {keycount} APIs attached')
 # get the APIs
 apis = tykInstance.getAPIs()
 #print(json.dumps(apis.json(), indent=2, sort_keys=True))
-if verbose:
+if args.verbose:
     keycount = len(apis.json())
     print(f'A total of {keycount} APSs are defined')
 addedCount = 0
@@ -62,10 +40,10 @@ for api in apis.json()["apis"]:
     #print(json.dumps(api, indent=2, sort_keys=True))
     apiid=api["api_id"]
     apiName = api["name"]
-    if addedCount < toAdd:
+    if addedCount < args.toAdd:
         if not apiid in policy["access_rights"]:
             #print(json.dumps(policy["access_rights"], indent=2, sort_keys=True))
-            if verbose:
+            if args.verbose:
                 print(f'Adding {apiid}, {apiName}')
             policy["access_rights"][apiid] = {
                 "api_name": apiName,
@@ -87,14 +65,14 @@ for api in apis.json()["apis"]:
             })
             addedCount+=1
         else:
-            if verbose:
+            if args.verbose:
                 print(f'Skipping {apiid}, {apiName}, already attached')
     else:
         continue
-if addedCount < toAdd:
+if addedCount < args.toAdd:
     print(f'Only able to add {addedCount} APIs because because there too fews APIs defined')
 print(f'Policy {policyID} will have a total of {len(policy["access_rights"])} APIs attached')
-if verbose:
+if args.verbose:
     print(json.dumps(policy, indent=2, sort_keys=True))
 print("Uploading policy to dashboard")
 resp = tykInstance.updatePolicy(json.dumps(policy), policyID)
