8519767d132f2d2fd3dba697cba0f6912e2bd1ec
Peter Stubbs <peter@tyk.io>
2024-07-12 09:50:32 +0100

    deleteOrganisation.py: added

---
+++ python/deleteOrganisation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+###############################################################
+# Reference for calling the dashboard.deleteOrganisation
+# arguments are dashboard and dashboard admin secret
+###############################################################
+
+import argparse
+import json
+import os
+import sys
+sys.path.append(f'{os.path.abspath(os.path.dirname(__file__))}/module')
+import tyk
+
+scriptName = os.path.basename(__file__)
+
+parser = argparse.ArgumentParser(description=f'{scriptName}: Delete the org with the given orgid')
+
+parser.add_argument('-d', '--dashboard', required=True, dest='dshb', help="URL of the dashboard")
+parser.add_argument('-a', '--adminsecret', required=True, dest='adminsecret', help="Dashboard admin secret")
+parser.add_argument('-o', '--orgid', required=True, dest='orgid', nargs='+', help="Orgid to retrieve")
+parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help="Verbose output")
+
+args = parser.parse_args()
+
+tykInstance = tyk.dashboard(args.dshb, "", args.adminsecret)
+
+for org in args.orgid:
+    print(f'Deleting {org=}')
+    resp = tykInstance.deleteOrganisation(org)
+    print(json.dumps(resp.json(), indent=2))
+    if resp.status_code != 200:
+        sys.exit(1)
