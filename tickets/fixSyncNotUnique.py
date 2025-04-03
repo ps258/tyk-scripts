65eec397b493450acf989a48aa8fb38b87374231
Peter Stubbs <peter@tyk.io>
2025-04-03 08:54:08 +0100

    fixSyncNotUnique.py: added support for OAS APIs

--- tickets/fixSyncNotUnique.py
+++ tickets/fixSyncNotUnique.py
@@ -7,53 +7,103 @@
 import sys
 import json
 
-allnames = dict()
-allslugs = dict()
-alllisten_paths = dict()
+allNames = dict()
+allSlugs = dict()
+allListenPaths = dict()
+
+def getNewName(name, allNames):
+    if name in allNames:
+        i = 1
+        while name+str(i) in allNames:
+            i += 1
+        newname = name+str(i)
+        allNames[newname] = 1
+        return newname
+    allNames[name] = 1
+    return name
+
+def getNewSlug(slug, allSlugs):
+    if slug in allSlugs:
+        i = 1
+        while slug+str(i) in allSlugs:
+            i += 1
+        newslug = slug+str(i)
+        allSlugs[newslug] = 1
+        return newslug
+    allSlugs[slug] = 1
+    return slug
+
+def getNewListenPath(listenPath, allListenPaths):
+    if listenPath in allListenPaths:
+        i = 1
+        while listenPath+str(i) in allListenPaths:
+            i += 1
+        newListenPath = listenPath+str(i)
+        allListenPaths[newListenPath] = 1
+        return newListenPath
+    allListenPaths[listenPath] = 1
+    return listenPath
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2 :
+        print(f"[USAGE]{sys.argv[0]}: api* oas*")
+        print(f"       It is necessary to specify both the path to the api* and oas* files at the same time so that the listen paths can be made unique")
+        sys.exit(1)
     for i, file in enumerate(sys.argv[1:]):
         with open(file, "r") as APIFile:
             APIjson=json.load(APIFile)
         changed = False
-        name = APIjson["api_definition"]["name"]
-        slug = APIjson["api_definition"]["slug"]
-        listen_path = APIjson["api_definition"]["proxy"]["listen_path"]
-        i = 1
-        #print(f'{file}: {name}, {slug}, {listen_path}')
-        if name in allnames:
+        if "api_definition" in APIjson:
+            name = APIjson["api_definition"]["name"]
+            slug = APIjson["api_definition"]["slug"]
+            listenPath = APIjson["api_definition"]["proxy"]["listen_path"]
+            #print(f'{file}: {name}, {slug}, {listenPath}')
+
             # find the next free name
-            while name+str(i) in allnames:
-                i += 1
-            newname = name+str(i)
-            allnames[newname] = 1
-            print(f'File: {file} Name Clash: {name} -> {newname}')
-            APIjson["api_definition"]["name"] = newname
-            changed = True
-        else:
-            allnames[name] = 1
+            newname = getNewName(name, allNames)
+            if name != newname:
+                print(f'File: {file} Name Clash: {name} -> {newname}')
+                APIjson["api_definition"]["name"] = newname
+                changed = True
 
-        if slug in allslugs:
-            while slug+str(i) in allslugs:
-                i += 1
-            newslug = slug+str(i)
-            allslugs[newslug] = 1
-            APIjson["api_definition"]["slug"] = newslug
-            changed = True
-            print(f'File: {file} Slug Clash: {slug} -> {newslug}')
-        else:
-            allslugs[slug] = 1
+            # find the next free slug
+            newslug = getNewSlug(slug, allSlugs)
+            if slug != newslug:
+                print(f'File: {file} Slug Clash: {slug} -> {newslug}')
+                APIjson["api_definition"]["slug"] = newslug
+                changed = True
 
-        if listen_path in alllisten_paths:
-            while listen_path+str(i) in alllisten_paths:
-                i += 1
-            newlisten_path = listen_path+str(i) 
-            alllisten_paths[newlisten_path] = 1
-            APIjson["api_definition"]["proxy"]["listen_path"] = newlisten_path
-            changed = True
-            print(f'File: {file} Listen Path Clash: {listen_path} -> {newlisten_path}')
+            # find the next free listen path
+            newListenPath = getNewListenPath(listenPath, allListenPaths)
+            if newListenPath != listenPath:
+                print(f'File: {file} Listen Path Clash: {listenPath} -> {newListenPath}')
+                APIjson["api_definition"]["proxy"]["listen_path"] = newListenPath
+                changed = True
+        
+        elif "oas" in APIjson:
+            name = APIjson["oas"]["x-tyk-api-gateway"]["info"]["name"]
+            listenPath = APIjson["oas"]["x-tyk-api-gateway"]["server"]["listenPath"]["value"]
+
+            # find the next free name
+            newname = getNewName(name, allNames)
+            if name != newname:
+                print(f'File: {file} Name Clash: {name} -> {newname}')
+                APIjson["oas"]["x-tyk-api-gateway"]["info"]["name"] = newname
+                changed = True
+
+            # find the next free listen path
+            newListenPath = getNewListenPath(listenPath, allListenPaths)
+            if newListenPath != listenPath:
+                print(f'File: {file} Listen Path Clash: {listenPath} -> {newListenPath}')
+                APIjson["oas"]["x-tyk-api-gateway"]["server"]["listenPath"]["value"] = newListenPath
+                changed = True
+        
+        elif "oas" in APIjson:
+            name = APIjson["oas"]["x-tyk-api-gateway"]["info"]["name"]
+            listenPath = APIjson["oas"]["x-tyk-api-gateway"]["server"]["listenPath"]["value"]
+
         else:
-            alllisten_paths[listen_path] = 1
+            print(f"[WARN]{file=} is not in Tyk Classic or Tyk OAS format")
 
         if changed:
             with open(file, "w") as APIFile:
