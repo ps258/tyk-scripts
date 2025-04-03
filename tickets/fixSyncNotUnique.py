69ee51860155ec1b67e7e1ca8608e3432f87506b
Peter Stubbs <peter@tyk.io>
2025-04-03 11:18:22 +0100

    fixSyncNotUnique.py: unified the name, listenpath and slug "new" functions

--- tickets/fixSyncNotUnique.py
+++ tickets/fixSyncNotUnique.py
@@ -11,38 +11,16 @@ allNames = dict()
 allSlugs = dict()
 allListenPaths = dict()
 
-def getNewName(name, allNames):
-    if name in allNames:
+def getNewEntry(name, allEntries):
+    if name in allEntries:
         i = 1
-        while name+str(i) in allNames:
+        while name+str(i) in allEntries:
             i += 1
         newname = name+str(i)
-        allNames[newname] = 1
+        allEntries[newname] = 1
         return newname
-    allNames[name] = 1
+    allEntries[name] = 1
     return name
-
-def getNewSlug(slug, allSlugs):
-    if slug in allSlugs:
-        i = 1
-        while slug+str(i) in allSlugs:
-            i += 1
-        newslug = slug+str(i)
-        allSlugs[newslug] = 1
-        return newslug
-    allSlugs[slug] = 1
-    return slug
-
-def getNewListenPath(listenPath, allListenPaths):
-    if listenPath in allListenPaths:
-        i = 1
-        while listenPath+str(i) in allListenPaths:
-            i += 1
-        newListenPath = listenPath+str(i)
-        allListenPaths[newListenPath] = 1
-        return newListenPath
-    allListenPaths[listenPath] = 1
-    return listenPath
 
 if __name__ == "__main__":
     if len(sys.argv) < 2 :
@@ -60,21 +38,21 @@ if __name__ == "__main__":
             #print(f'{file}: {name}, {slug}, {listenPath}')
 
             # find the next free name
-            newname = getNewName(name, allNames)
+            newname = getNewEntry(name, allNames)
             if name != newname:
                 print(f'File: {file} Name Clash: {name} -> {newname}')
                 APIjson["api_definition"]["name"] = newname
                 changed = True
 
             # find the next free slug
-            newslug = getNewSlug(slug, allSlugs)
+            newslug = getNewEntry(slug, allSlugs)
             if slug != newslug:
                 print(f'File: {file} Slug Clash: {slug} -> {newslug}')
                 APIjson["api_definition"]["slug"] = newslug
                 changed = True
 
             # find the next free listen path
-            newListenPath = getNewListenPath(listenPath, allListenPaths)
+            newListenPath = getNewEntry(listenPath, allListenPaths)
             if newListenPath != listenPath:
                 print(f'File: {file} Listen Path Clash: {listenPath} -> {newListenPath}')
                 APIjson["api_definition"]["proxy"]["listen_path"] = newListenPath
@@ -85,14 +63,14 @@ if __name__ == "__main__":
             listenPath = APIjson["oas"]["x-tyk-api-gateway"]["server"]["listenPath"]["value"]
 
             # find the next free name
-            newname = getNewName(name, allNames)
+            newname = getNewEntry(name, allNames)
             if name != newname:
                 print(f'File: {file} Name Clash: {name} -> {newname}')
                 APIjson["oas"]["x-tyk-api-gateway"]["info"]["name"] = newname
                 changed = True
 
             # find the next free listen path
-            newListenPath = getNewListenPath(listenPath, allListenPaths)
+            newListenPath = getNewEntry(listenPath, allListenPaths)
             if newListenPath != listenPath:
                 print(f'File: {file} Listen Path Clash: {listenPath} -> {newListenPath}')
                 APIjson["oas"]["x-tyk-api-gateway"]["server"]["listenPath"]["value"] = newListenPath
