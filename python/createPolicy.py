9873ef20e4efd7f0b8fd2d0cc9cda7da7d3fc4b8
Peter Stubbs <peter@tyk.io>
2025-11-11 11:21:36 +0000

    createPolicy.py: added description and some empty lines to make things easier to read

--- python/createPolicy.py
+++ python/createPolicy.py
@@ -9,11 +9,14 @@ import tyk
 
 scriptName = os.path.basename(__file__)
 
-description = "Will take the template, apply the name (if given) then add it as a policy to the dashboard or gateway"
+description = "Will take the template or create a policy in memory, apply the name (if given) then add it to the dashboard or gateway"
+
 parser = argparse.ArgumentParser(description=f'{scriptName}: {description}')
+
 DashboardOrGateway = parser.add_mutually_exclusive_group(required=True)
 DashboardOrGateway.add_argument('--dashboard', '-d', dest='dshb', help="URL of the dashboard")
 DashboardOrGateway.add_argument('--gateway', '-g', dest='gatw', help="URL of the gateway")
+
 parser.add_argument('--cred', '-c', required=True, dest='auth', help="Dashboard API key or Gateway secret")
 parser.add_argument('--name', '-n', default="Default policy name", dest='name', help="Base name of policy")
 parser.add_argument('--apiid', '-a', dest='apiids', nargs='+', help="List of API IDs to be added to the policy")
@@ -39,6 +42,7 @@ else:
     policy = tyk.policy()
 
 policy.setName(args.name)
+
 for apiid in args.apiids:
     policy.addAPI(apiid)
 
